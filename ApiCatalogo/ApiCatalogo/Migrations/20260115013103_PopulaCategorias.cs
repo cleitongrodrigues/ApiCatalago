22be1079f0a963a30817d990ab001fcc93728395
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2026-01-14 22:40:27 -0300

    criando migrations para popular as tabelas categorias e produtos

---
+++ ApiCatalogo/ApiCatalogo/Migrations/20260115013103_PopulaCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert Into Categorias (Nome, ImagemUrl) Values ('Bebida1','bebidas1.jpg')");
+            mb.Sql("Insert Into Categorias (Nome, ImagemUrl) Values ('Bebida2','bebidas2.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete From Categorias");
+        }
+    }
+}
---
+++ ApiCatalogo/ApiCatalogo/Migrations/20260115013532_PopulaProdutos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert Into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                   "Values ('Jhony Walker', 'Whisky', 175.50, 'walkerwhisky.jpg', 8, now(), 1)");
+
+            mb.Sql("Insert Into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                    "Values ('Royal Salut', 'Whisky', 585.50, 'salut.jpg', 3, now(), 2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete From Produtos");
+        }
+    }
+}
