12db08d28a52e4efe2e773bb2d7076128117b457
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2026-01-16 10:31:55 -0300

    Criando o controller de categorias

--- ApiCatalogo/ApiCatalogo/Program.cs
+++ ApiCatalogo/ApiCatalogo/Program.cs
@@ -1,11 +1,14 @@
 using ApiCatalogo.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+  options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); // UTILIZADO PARA OBTER DADOS DE DUAS ENTIDADES EM UM MESMO ENDPOINT
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
